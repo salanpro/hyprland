abc9458e81ea836bba360c0238866b205fb588ae
sergioalanpro <sergioalanpro@hotmail.com>
2025-08-17 18:43:42 -0600

    Hyprland update

---
+++ setup.sh
@@ -0,0 +1,40 @@
+
+echo "[+] Updating system and installing base packages..."
+sudo pacman -Syu
+sudo pacman -S pavucontrol gsimplecal pipewire-jack alsa-firmware alsa-utils nautilus alacritty alsa-utils base-devel brightnessctl fastfetch swaync firefox gnome-text-editor hyprland sddm waybar wofi hyprlock hypridle bluez blueman-manager hyprshot loupe hyprpaper numlockx vulkan-radeon mesa tlp tlp-rdw nwg-look ttf-fira-code noto-fonts ttf-jetbrains-mono
+
+
+echo "[+] Enabling services..."
+sudo systemctl enable sddm.service
+sudo systemctl enable tlp.service
+sudo systemctl enable upower.service
+sudo systemctl enable bluetooth.service
+
+echo "[+] Copying to .icons..."
+mkdir -p ~/.icons
+cp -r .icons/* ~/.icons/
+
+echo "[+] Copying to .config..."
+mkdir -p ~/.config
+cp -r .config/* ~/.config/
+
+echo "[+] Copying wallpaper..."
+mkdir -p ~/Pictures
+cp wallpaper/* ~/Pictures/
+
+echo "[+] Copying .bashrc..."
+cp -f .bashrc ~/.bashrc
+
+echo "[+] Making ~/gitclones folder..."
+mkdir -p ~/Downloads/gitclones && cd ~/Downloads/gitclones
+
+echo "[+] Installing dependencies for ble.sh..."
+sudo pacman -S --noconfirm make gawk
+
+echo "[+] Cloning and installing ble.sh..."
+git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
+make -C ble.sh install PREFIX=~/.local
+
+echo "[+] ble.sh installed in ~/.local/share/blesh/"
+
+echo "[✅] Setup complete! You can now reboot to apply all changes."
