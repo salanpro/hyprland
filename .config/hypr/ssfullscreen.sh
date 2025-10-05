f2291b88cdf6d6b1481f10d99f91b304ebe0f79f
sergioalanpro <sergioalanpro@hotmail.com>
2025-10-04 18:50:20 -0600

    deleted scripts

--- .config/hypr/ssfullscreen.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-pkill slurp || hyprshot -m output --raw \
-| satty --filename - \
---output-filename ~/Pictures/Screenshots/Screenshot-$(date +'%Y-%m-%d_%H-%M-%S').png \
---early-exit \
---copy-command 'wl-copy'
--- .config/hypr/sswindow.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-pkill slurp || hyprshot -m window --raw \
-| satty --filename - \
---output-filename ~/Pictures/Screenshots/Screenshot-$(date +'%Y-%m-%d_%H-%M-%S').png \
---early-exit \
---copy-command 'wl-copy'
