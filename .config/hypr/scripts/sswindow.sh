864236476d6872bb334a845273617ccebfc2469e
sergioalanpro <sergioalanpro@hotmail.com>
2025-10-09 17:13:13 -0600

    update

--- .config/hypr/scripts/sswindow.sh
+++ .config/hypr/scripts/sswindow.sh
@@ -4,4 +4,5 @@ pkill slurp || hyprshot -m window --raw \
 | satty --filename - \
 --output-filename ~/Pictures/Screenshots/Screenshot-$(date +'%Y-%m-%d_%H-%M-%S').png \
 --early-exit \
---copy-command 'wl-copy'
+--copy-command 'wl-copy' \
+--disable-notifications
