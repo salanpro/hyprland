19a48a424c3f29eb50ddae4592188ab63d7734fc
sergioalanpro <sergioalanpro@hotmail.com>
2025-11-11 19:27:09 -0600

    added tuigreet

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 echo "[+] Updating system and installing base packages..."
 sudo pacman -Syu
 sudo pacman -S showtime gst-libav libmpeg2 xdg-desktop-portal-hyprland pavucontrol gsimplecal wireplumber pipewire-jack pipewire-pulse alsa-firmware alsa-utils \
-nautilus alacritty base-devel brightnessctl fastfetch swaync firefox gnome-text-editor hyprland sddm waybar wofi hyprlock hypridle bluez blueman hyprshot \
-loupe hyprpaper numlockx vulkan-radeon mesa tlp tlp-rdw nwg-look ttf-fira-code noto-fonts ttf-jetbrains-mono ttf-jetbrains-mono-nerd satty showtime
+nautilus alacritty base-devel brightnessctl fastfetch swaync firefox gnome-text-editor hyprland waybar wofi hyprlock hypridle bluez blueman hyprshot \
+loupe hyprpaper numlockx vulkan-radeon mesa tlp tlp-rdw nwg-look ttf-fira-code noto-fonts ttf-jetbrains-mono ttf-jetbrains-mono-nerd satty showtime greetd-tuigreet
 
 echo "[+] Enabling services..."
-sudo systemctl enable sddm.service
+sudo systemctl enable greetd.service
 sudo systemctl enable tlp.service
 sudo systemctl enable bluetooth.service
 systemctl --user enable pipewire.service
@@ -20,9 +20,8 @@ echo "[+] Copying to .config..."
 mkdir -p ~/.config
 cp -r .config/* ~/.config/
 
-echo "[+] Setting sddm theme..."
-sudo cp -r sddm_theme/Irixium /usr/share/sddm/themes
-sudo cp sddm_theme/sddm.conf /etc/sddm.conf
+echo "[+] Setting tuigreet..."
+sudo cp tuigreet/config.toml /etc/greetd/
 
 echo "[+] Copying wallpaper..."
 mkdir -p ~/Pictures/wallpaper/
