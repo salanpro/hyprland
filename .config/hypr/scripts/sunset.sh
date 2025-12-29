f0c1f81155942db9dc7d0bbda39136a65ede3460
salanpro <salanpro@proton.me>
2025-12-29 00:53:55 -0600

    added hyprsunset

--- .config/hypr/scripts/sunset.sh
+++ .config/hypr/scripts/sunset.sh
@@ -5,5 +5,5 @@ if pgrep -x "hyprsunset" > /dev/null; then
     notify-send "Hyprsunset" "Desactivado"
 else
     hyprsunset --temperature 5000 &
-    notify-send "Hyprsunset" "Activado (3000K)"
+    notify-send "Hyprsunset" "Activado (5000K)"
 fi
