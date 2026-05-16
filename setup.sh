2baffc221f62b72028a2504ab8f38d16d498dcc8
Sergio Alan <189182351+salanpro@users.noreply.github.com>
2026-05-16 15:06:38 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -31,7 +31,8 @@ sudo pacman -Syu
 sudo pacman -S showtime gst-libav libmpeg2 xdg-desktop-portal-hyprland pavucontrol wireplumber pipewire-jack pipewire-pulse alsa-firmware alsa-utils \
 nautilus alacritty base-devel brightnessctl fastfetch swaync firefox hyprland waybar hyprlock hypridle bluez blueman hyprshot \
 loupe hyprpaper numlockx vulkan-radeon mesa tlp tlp-rdw nwg-look ttf-fira-code noto-fonts ttf-jetbrains-mono ttf-jetbrains-mono-nerd satty showtime \
-greetd-tuigreet fzf wl-clipboard xdg-desktop-portal-gtk decibels nvim gnome-calendar hyprpicker noto-fonts-emoji papers hyprsunset syntax-highlighting rsync neovide
+greetd-tuigreet fzf wl-clipboard xdg-desktop-portal-gtk decibels nvim gnome-calendar hyprpicker noto-fonts-emoji papers hyprsunset syntax-highlighting rsync neovide \
+qt6-shadertools
 
 echo "[+] Enabling services..."
 sudo systemctl enable greetd.service
